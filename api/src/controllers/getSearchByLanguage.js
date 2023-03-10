ebd5bc69926164867c40a01fa919fd200d7006f9
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-03-10 18:08:02 -0300

    route handler controller API get on board  busqueda por tipo de lenguaje

---
+++ api/src/controllers/getSearchByLanguage.js
@@ -0,0 +1,19 @@
+const searchQueryOnBoardAPI = require('../handlers/searchQueryOnBoardAPI')
+
+const getSearchByLanguage = async (search) => {
+    try {
+        const jobs = await searchQueryOnBoardAPI(search)
+        
+        return jobs.data.map(job => {
+            return {
+                title: job.attributes. title,
+                description: job.attributes.description
+            } } )
+
+    } catch (error) {
+        return error
+    }
+}
+
+module.exports = getSearchByLanguage;
+
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,11 @@
 //ACA IMPORTAN Y SE DEFINEN LAS RUTAS
 const { Router } = require('express')
 const { allUsers } = require('./userRoute')
+const { homeRoute } = require('./homeRoute')
 
 const route = Router()
 
 route.get('/users', allUsers)
+route.get('/home' , homeRoute)
 
 module.exports = route
