bf27215594c66a938ad6ece193ae9030def184f0
Felipe <felipe.calderon321@gmail.com>
2023-03-11 23:00:50 -0300

    company controller

--- api/src/routes/homeRoute.js
+++ api/src/routes/homeRoute.js
@@ -1,18 +1,23 @@
 
-const getSearchByLanguage = require('../controllers/getSearchByLanguage')
+const {getSearchByLanguage, getCompanyById} = require('../controllers/getSearchByLanguage')
 
 const homeRoute = async (req, res) => {
     try {
-        const {language} = req.query
-        console.log(language)
-        
-        if (language) {
-            return res.status(200).send(await getSearchByLanguage(language))
-        }
+        const dataApi = await getSearchByLanguage(req.query)
+        return res.status(200).json(dataApi)
         
     } catch (error) {
-        res.status(404).send(error)
+        res.status(404).json(error)
     }
 }
 
-module.exports = {homeRoute}
+const companiesRoute = async (req, res) => {
+    try {
+        const dataApi = await getCompanyById(req.params)
+        return res.status(200).json(dataApi)
+        
+    } catch (error) {
+        res.status(404).json(error)
+    }
+}
+module.exports = {homeRoute, companiesRoute}
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,12 @@
 //ACA IMPORTAN Y SE DEFINEN LAS RUTAS
 const { Router } = require('express')
 const { allUsers } = require('./userRoute')
-const { homeRoute } = require('./homeRoute')
+const { homeRoute, companiesRoute } = require('./homeRoute')
 
 const route = Router()
 
 route.get('/users', allUsers)
 route.get('/home' , homeRoute)
+route.get('/company/:id', companiesRoute)
 
 module.exports = route
