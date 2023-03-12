b542a8b0cbb162917dbffd1dfb9e5db39c9c27bb
Felipe <felipe.calderon321@gmail.com>
2023-03-11 23:00:50 -0300

    company controller

--- api/src/controllers/getSearchByLanguage.js
+++ api/src/controllers/getSearchByLanguage.js
@@ -1,13 +1,19 @@
-const searchQueryOnBoardAPI = require('../handlers/searchQueryOnBoardAPI')
+const {searchQueryOnBoardAPI , searchCompaniesAPI} = require('../handlers/searchQueryOnBoardAPI')
 
-const getSearchByLanguage = async (search) => {
+const getSearchByLanguage = async ({language}) => {
     try {
-        const jobs = await searchQueryOnBoardAPI(search)
-        
-        return jobs.data.map(job => {
+        if(!language) throw 'debe agregar un language en query'
+
+        const jobs = await searchQueryOnBoardAPI(language)
+
+        return jobs.data.map((job) => {
             return {
                 title: job.attributes.title,
-                description: job.attributes.description
+                description: job.attributes.description,
+                remoto: job.attributes.remote,
+                postulantes: job.attributes.applications_count,
+                link: job.links.public_url,
+                idEmpresa: job.attributes.company.data.id
             } } )
 
     } catch (error) {
@@ -15,5 +21,27 @@ const getSearchByLanguage = async (search) => {
     }
 }
 
-module.exports = getSearchByLanguage;
+const getCompanyById = async ({id}) => {
+    try {
+        const companies = await searchCompaniesAPI(id)
+        let social = {
+            facebook: companies.facebook !== '' ? companies.facebook : null,
+            twitter: companies.twitter !== '' ? companies.twitter : null,
+            github: companies.github !== '' ? companies.github : null,
+        }
+
+        return {
+            name: companies.name,
+            description: companies.description,
+            benefits: companies.benefits,
+            web: companies.web,
+            logo: companies.logo,
+            social
+        }
+    } catch (error) {
+        return error
+    }
+}
+
+module.exports = { getSearchByLanguage, getCompanyById};
 
