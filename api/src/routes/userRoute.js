697ebd068b3fb8736a0b806365a2567732c225b6
Felipe <felipe.calderon321@gmail.com>
2023-03-13 12:41:12 -0300

    user model added

--- api/src/routes/userRoute.js
+++ api/src/routes/userRoute.js
@@ -1,5 +1,13 @@
-const allUsers = (req, res) => {
-    res.json({msje: 'ok'})
+const {getUsers} = require('../controllers/userModel')
+
+const allUsers = async (req, res) => {
+    try {
+        const data = await(getUsers)
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+
 }
 
 module.exports = {allUsers}
