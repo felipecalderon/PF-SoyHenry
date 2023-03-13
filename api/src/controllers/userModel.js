c1dc4306bdd62b4f0968077c36b7432561929ea1
Felipe <felipe.calderon321@gmail.com>
2023-03-13 16:25:18 -0300

    user model from will

--- api/src/controllers/userModel.js
+++ api/src/controllers/userModel.js
@@ -1,12 +1,95 @@
 const User = require('../models/userModel')
 
-const getUsers = async () => {
+const createUsersController = async (  ) => {
+    // Trae los datos del cuepo de la req.
+    const { nombres, apellidos, celular, correo, discapacidad, genero } = req.body;
     try {
-        const dataUser = await User.findAll()
-        return dataUser
+        const newUser = await createUsers( nombres, apellidos, celular, correo, discapacidad, genero );
+        return newUser
     } catch (error) {
-        throw error
+        throw { error: error.message }
     }
+};
+
+// Gets
+const getUsersControllers = async (  ) => {
+    const { name } = req.query;
+    try {
+        const response = name ? await getUsersByName( name ) : await getUsers();
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+const getUsersByIdControllers = async (  ) => {
+    const { id } = req.params;
+    try {
+        const response = await getUsersById( id );
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+const getUsersInactControllers = async (  ) => {
+    try {
+        const response = await getUsersInact();
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+const getUsersInactByIdControllers = async (  ) => {
+    const { id } = req.params;
+    try {
+        const response = await getUsersById( id );
+        return response 
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+
+// Puts
+const putUsersControllers = async (  ) => {
+    const { id } = req.params;
+    const { nombres, apellidos, celular, correo, discapacidad, genero } = req.body;
+    try {
+        const response = await putUsers( id, nombres, apellidos, celular, correo, discapacidad, genero );
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+const putStateControllers = async (  ) => {
+    const { id } = req.params;
+    const { estado } = req.body;
+    try {
+        const response = await putState( id, estado );
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+
+// Delete
+const deleteUsersControllers = async (  ) => {
+    const { id } = req.params;
+    try {
+        const response = await deleteUsers( id );
+        return response
+    } catch (error) {
+        throw { error: error.message }
+    }
+};
+
+module.exports = {
+    createUsersController,
+    getUsersControllers,
+    getUsersByIdControllers,
+    getUsersInactControllers,
+    getUsersInactByIdControllers,
+    putUsersControllers,
+    putStateControllers,
+    deleteUsersControllers
 }
 
 module.exports = { getUsers }
