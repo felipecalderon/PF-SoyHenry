8448283390cada121d208327d4cbd4f692e72360
Felipe <felipe.calderon321@gmail.com>
2023-03-13 16:27:09 -0300

    user controller from will

--- api/src/controllers/userModel.js
+++ api/src/controllers/userModel.js
@@ -1,8 +1,7 @@
 const User = require('../models/userModel')
 
-const createUsersController = async (  ) => {
+const createUsersController = async ( { nombres, apellidos, celular, correo, discapacidad, genero } ) => {
     // Trae los datos del cuepo de la req.
-    const { nombres, apellidos, celular, correo, discapacidad, genero } = req.body;
     try {
         const newUser = await createUsers( nombres, apellidos, celular, correo, discapacidad, genero );
         return newUser
@@ -12,8 +11,7 @@ const createUsersController = async (  ) => {
 };
 
 // Gets
-const getUsersControllers = async (  ) => {
-    const { name } = req.query;
+const getUsersControllers = async ( { name } ) => {
     try {
         const response = name ? await getUsersByName( name ) : await getUsers();
         return response
@@ -21,8 +19,7 @@ const getUsersControllers = async (  ) => {
         throw { error: error.message }
     }
 };
-const getUsersByIdControllers = async (  ) => {
-    const { id } = req.params;
+const getUsersByIdControllers = async ( { id } ) => {
     try {
         const response = await getUsersById( id );
         return response
@@ -30,7 +27,7 @@ const getUsersByIdControllers = async (  ) => {
         throw { error: error.message }
     }
 };
-const getUsersInactControllers = async (  ) => {
+const getUsersInactControllers = async () => {
     try {
         const response = await getUsersInact();
         return response
@@ -38,8 +35,7 @@ const getUsersInactControllers = async (  ) => {
         throw { error: error.message }
     }
 };
-const getUsersInactByIdControllers = async (  ) => {
-    const { id } = req.params;
+const getUsersInactByIdControllers = async ( { id } ) => {
     try {
         const response = await getUsersById( id );
         return response 
@@ -49,8 +45,7 @@ const getUsersInactByIdControllers = async (  ) => {
 };
 
 // Puts
-const putUsersControllers = async (  ) => {
-    const { id } = req.params;
+const putUsersControllers = async ( { id } ) => {
     const { nombres, apellidos, celular, correo, discapacidad, genero } = req.body;
     try {
         const response = await putUsers( id, nombres, apellidos, celular, correo, discapacidad, genero );
@@ -59,8 +54,7 @@ const putUsersControllers = async (  ) => {
         throw { error: error.message }
     }
 };
-const putStateControllers = async (  ) => {
-    const { id } = req.params;
+const putStateControllers = async ( { id } ) => {
     const { estado } = req.body;
     try {
         const response = await putState( id, estado );
@@ -71,8 +65,7 @@ const putStateControllers = async (  ) => {
 };
 
 // Delete
-const deleteUsersControllers = async (  ) => {
-    const { id } = req.params;
+const deleteUsersControllers = async ( { id } ) => {
     try {
         const response = await deleteUsers( id );
         return response
