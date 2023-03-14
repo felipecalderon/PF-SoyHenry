db0eb4a5b2ef071a8a3b2a1096a938931930597e
Ericp95 <ericp6960@gmail.com>
2023-03-14 15:45:58 -0300

    creacion admin y sus componentes eric

--- api/src/models/adminModel.js
+++ api/src/models/adminModel.js
@@ -7,6 +7,31 @@ const Admin = sequelize.define('Admin', {
     autoIncrement: true,
     primaryKey: true,
   },
+  aptitudes: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  usuarios: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  empresas: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  publicaciones: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  rol: {
+    type: DataTypes.ENUM("Admin", "Postulante", "Empresa"),
+    allowNull: false,
+  },
+  activo:{
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
   // otros campos específicos de Admin
 });
 
