045d39b96313669bde618b680aa22febdb9d3a72
Santiago Pagge <santy.pagge@gmail.com>
2023-03-15 10:39:07 -0300

    Modal funciona falta estilar

--- client/src/components/ModalLogin/ModalLogin.jsx
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -1,14 +1,27 @@
 import React from "react"
 
-export const ModalLogin = ({children, isOpen, closeModal}) => {
+export const ModalLogin = ({isOpen, setOpen}) => {
 
-    const handleModalContainerClick = (event) => event.stopPropagation()
+    const closeModal = () => {
+        setOpen(false)
+    }
 
+    const handleModalContainerClick = (event) => event.stopPropagation() //.stopPropagation hace que no se cierre el modal al hacer click dentro
+
+    if(!isOpen) return null
     return (
-        <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
-            <div className="modal-container" onClick={handleModalContainerClick}>
-                <button className="modal-close" onClick={closeModal}>X</button>
-                {children}
+        <article className='flex justify-center' onClick={closeModal}>
+            <div className="bg-yellow-200 w-72 h-72 modal-container" onClick={handleModalContainerClick}>
+                <button className="flex relative hover:text-yellow-200 font-medium text-2xl px-4 py-0 mb-4 mt-2">Ingresar a Fusionajob</button>
+
+                <label htmlFor='username' className='px-4'>Usuario: </label>
+                <input type='text' name='username' className='mb-2'></input>
+                
+                <label htmlFor='password' className='px-1'>Contraseña: </label>
+                <input type='password' name='password' className='mb-2'></input>
+
+                <button className="flex hover:text-yellow-200 font-medium px-4 py-2">Ingresar</button>
+                <button className="flex hover:text-yellow-200 font-medium px-4 py-2">No tengo cuenta</button>
             </div>
         </article>
     )
