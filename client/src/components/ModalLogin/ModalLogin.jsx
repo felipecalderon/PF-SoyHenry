25a4c541ca9b8bc9ecfb5bb176e6371f4ca4b750
Santiago Pagge <santy.pagge@gmail.com>
2023-03-15 15:59:46 -0300

    maquetado modal

--- client/src/components/ModalLogin/ModalLogin.jsx
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -10,19 +10,23 @@ export const ModalLogin = ({isOpen, setOpen}) => {
 
     if(!isOpen) return null
     return (
-        <article className='flex justify-center' onClick={closeModal}>
-            <div className="bg-yellow-200 w-72 h-72 modal-container" onClick={handleModalContainerClick}>
-                <button className="flex relative hover:text-yellow-200 font-medium text-2xl px-4 py-0 mb-4 mt-2">Ingresar a Fusionajob</button>
-
-                <label htmlFor='username' className='px-4'>Usuario: </label>
-                <input type='text' name='username' className='mb-2'></input>
-                
-                <label htmlFor='password' className='px-1'>Contraseña: </label>
-                <input type='password' name='password' className='mb-2'></input>
-
-                <button className="flex hover:text-yellow-200 font-medium px-4 py-2">Ingresar</button>
-                <button className="flex hover:text-yellow-200 font-medium px-4 py-2">No tengo cuenta</button>
-            </div>
-        </article>
+        <div className='relative'>
+            <article className='flex justify-center' onClick={closeModal}>
+                <div className="bg-yellow-200 w-96 h-72 rounded-3xl" onClick={handleModalContainerClick}>
+                    <h3 className="flex justify-center font-medium text-2xl px-4 py-0 mb-2 mt-2 drop-shadow-md">Ingresar a Fusionajob</h3>
+                <div className='flex justify-center items-center'>
+                    <input type='text' name='username' placeholder="Email:" className='mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <input type='password' name='password' placeholder="Contraseña:" className='flex mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
+                </div>
+                    <button><img src=''/></button>
+                    <button></button>
+                    <button></button>
+                    <button className='flex hover:text-yellow-200 font-medium px-40 py-2'>Ingresar</button>
+                    <button className='flex hover:text-gray-400 font-medium text-sm pl-32 py-2'>No tengo cuenta</button>
+                </div>
+            </article>
+        </div>
     )
 }
