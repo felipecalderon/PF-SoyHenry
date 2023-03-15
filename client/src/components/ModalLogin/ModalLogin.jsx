db621b0853c64fdc27358604511643e87f7348dc
Santiago Pagge <santy.pagge@gmail.com>
2023-03-15 20:59:21 -0300

    modal listo para darle funcionalidad

--- client/src/components/ModalLogin/ModalLogin.jsx
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -1,4 +1,8 @@
 import React from "react"
+import facebook from '../../assets/facebook.png'
+import googlelogo from '../../assets/googlelogo.png'
+import github from '../../assets/github.png'
+
 
 export const ModalLogin = ({isOpen, setOpen}) => {
 
@@ -10,21 +14,27 @@ export const ModalLogin = ({isOpen, setOpen}) => {
 
     if(!isOpen) return null
     return (
-        <div className='relative'>
+        <div className='relative mt-5'>
             <article className='flex justify-center' onClick={closeModal}>
-                <div className="bg-yellow-200 w-96 h-72 rounded-3xl" onClick={handleModalContainerClick}>
-                    <h3 className="flex justify-center font-medium text-2xl px-4 py-0 mb-2 mt-2 drop-shadow-md">Ingresar a Fusionajob</h3>
-                <div className='flex justify-center items-center'>
-                    <input type='text' name='username' placeholder="Email:" className='mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
-                </div>
-                <div className='flex justify-center items-center'>
-                    <input type='password' name='password' placeholder="Contraseña:" className='flex mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
-                </div>
-                    <button><img src=''/></button>
-                    <button></button>
-                    <button></button>
-                    <button className='flex hover:text-yellow-200 font-medium px-40 py-2'>Ingresar</button>
-                    <button className='flex hover:text-gray-400 font-medium text-sm pl-32 py-2'>No tengo cuenta</button>
+                <div className="bg-yellow-200 w-96 h-auto rounded-3xl" onClick={handleModalContainerClick}>
+                        <h3 className="text-center font-medium text-2xl px-4 py-0 mb-2 mt-2 drop-shadow-md">Ingresar a Fusionajob</h3>
+                    <form>
+                        <div className='flex justify-center items-center'>
+                            <input type='text' name='username' placeholder="Email:" className='mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
+                        </div>
+                        <div className='flex justify-center items-center'>
+                            <input type='password' name='password' placeholder="Contraseña:" className='mb-2 border-8 rounded-2xl w-80 h-12 px-2'></input>
+                        </div>
+                    </form>
+                    <div className='flex ml-14'>
+                        <button className='w-8 pt-32 mx-6'><img src={facebook} alt='flogo'/></button>
+                        <button className='w-8 pt-32 mx-6'><img src={googlelogo} alt='glogo'/></button>
+                        <button className='w-8 pt-32 mx-6'><img src={github} alt='ghlogo'/></button>
+                    </div>
+                    <div className='flex flex-col w-auto'>
+                        <button className='hover:text-yellow-200 font-medium mx-auto w-full py-2 bg-indigo-600'>Ingresar</button>
+                        <button className='hover:text-gray-400 font-medium text-sm mx-auto w-full py-2 bg-lime-900'>No tengo cuenta</button>
+                    </div>
                 </div>
             </article>
         </div>
--- client/src/components/NavLanding/NavLanding.jsx
+++ client/src/components/NavLanding/NavLanding.jsx
@@ -30,9 +30,9 @@ export const NavLanding = () => {
             <Link href="#" className="inline-block hover:text-yellow-200 font-medium px-4 py-2">Planes/Tarifas</Link>
             <Link href="#" className="inline-block hover:text-yellow-200 font-medium px-4 py-2">About Us</Link>
             <Link href="#" className="inline-block hover:text-yellow-200 font-medium px-4 py-2">Registro</Link>
-            <Link href="#" className="inline-block hover:text-yellow-200 font-medium px-4 py-2">Ingreso</Link>
+            <Link href="#" onClick={() => {setOpen(!open)}} className="inline-block hover:text-yellow-200 font-medium px-4 py-2">Ingreso</Link>
           <div className="ml-6">
-            <button onClick={handleToggle} id="toggleButton" class="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">{isDarkMode ? 'Día' : 'Noche'}</button>
+            <button onClick={handleToggle} id="toggleButton" className="py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">{isDarkMode ? 'Día' : 'Noche'}</button>
           </div>
         </div>
       </div>
