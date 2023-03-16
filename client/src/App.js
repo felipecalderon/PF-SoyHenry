5727019d5b68f3f1375132364883c9faff385fb2
Santiago Pagge <santy.pagge@gmail.com>
2023-03-16 12:52:56 -0300

    dale que quiero mergear

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,10 @@ import Cards from './components/Cards/Cards'
 import LandingPage from './components/LandingPage/LandingPage';
 import JobDetail from './components/JobDetail/JobDetail';
 
+
+
+
+
 function App() {
   return (
     <div className={style.all}>
