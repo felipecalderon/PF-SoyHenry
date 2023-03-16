7a5fef524459b45e8833b2d9db4ae95d027668bb
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-03-16 12:01:23 -0300

    componente crear ofertas laborales con estilos basicos

--- client/src/components/Form/FormCreateOffers.jsx
+++ client/src/components/Form/FormCreateOffers.jsx
@@ -2,16 +2,17 @@ import React, {useState} from "react";
 import { useDispatch } from "react-redux";
 import { createOffer } from "../../redux/actions/postFetchOffers";
 import { Link } from "react-router-dom";
+import styles from './FormCreateOffers.module.css'
 
 
 function validate(inputs){
     let errors = {};
-    if (!inputs.title) errors.title = 'se requiere un titulo';
-    if (!inputs.description) errors.description = 'se requiere una descripcion';
-    if (!inputs.benefits) errors.benefits = 'se requiere beneficios';
-    if (!inputs.modality) errors.modality = 'debe selecionar una modalidad';
-    if (inputs.min_salary <= 0) errors.min_salary = 'ingrese un salario correcto';
-    if (inputs.max_salary <= 0) errors.max_salary = 'ingrese un salario correcto';
+    if (!inputs.title) errors.title = 'se requiere un titulo *';
+    if (!inputs.description) errors.description = 'se requiere una descripcion *';
+    if (!inputs.benefits) errors.benefits = 'se requiere beneficios *';
+    if (!inputs.modality) errors.modality = 'debe selecionar una modalidad *';
+    if (inputs.min_salary <= 0) errors.min_salary = 'ingrese un salario correcto *';
+    if (inputs.max_salary <= 0) errors.max_salary = 'ingrese un salario correcto *';
 
     return errors
 }
@@ -49,28 +50,29 @@ export default function OffersCreate () {
         dispatch(createOffer(inputs))
         alert('oferta creada')
     }
-
+    
     return(        
-        <div className="">
-            <h1 className="">Publicacion oferta laboral</h1>
+        <div className={styles.contenedor}>
+            <h1 className={styles.titulo}>Publicacion oferta laboral</h1>
             <form className="" onSubmit={(event) => handleSubmit(event)}>
-                <div className="">
+                <div className={styles.contenedor_inputs}>
+            {errors.title && <p>{errors.title} </p> }
                     <label >Titulo: </label>
-                    <input className="" type='text' onChange={(event) => handleChange(event)} value={inputs.title} name='title'  placeholder="Ingrese un titulo de la oferta"/>                        
-                    {errors.title && <p>{errors.title} </p> }
+                    <input className={styles.inputs_strings} type='text' onChange={(event) => handleChange(event)} value={inputs.title} name='title'  placeholder="Ingrese un titulo de la oferta"/>                        
                 </div>
-                <div className="">
-                    <label >Descripcion: </label>
-                    <input className="" type='text' onChange={(event) => handleChange(event)} value={inputs.description} name='description'  placeholder="ingrese requerimientos para aplicar a la oferta"/>                        
+                <div className={styles.contenedor_inputs}>
                     {errors.description && <p>{errors.description} </p> }
+                    <label >Descripcion: </label>
+                    <textarea className={styles.inputs_strings}  type='text' onChange={(event) => handleChange(event)} value={inputs.description} name='description'  placeholder="Describa la oferta laboral"/>                        
                 </div>               
-                <div className="">
-                    <label >Beneficios: </label>
-                    <input className="" type='text' onChange={(event) => handleChange(event)} value={inputs.benefits} name='benefits'  placeholder="ingrese los benefecios de pertenecer a la empresa"/>                        
+                <div className={styles.contenedor_inputs}>
                     {errors.benefits && <p>{errors.benefits} </p>}
+                    <label >Beneficios: </label>
+                    <textarea className={styles.inputs_strings}  type='text' onChange={(event) => handleChange(event)} value={inputs.benefits} name='benefits'  placeholder="ingrese los benefecios de pertenecer a la empresa"/>                        
                 </div>
 
-                <div className="">
+                <div className={styles.contenedor_inputs}>
+                    {errors.modality && <p>{errors.modality} </p>}            
                     <label >Modalidad: </label>
                     <select name="modality" onChange={(event) => handleChange(event)} >
                         <option value="">Seleccione</option>
@@ -78,23 +80,24 @@ export default function OffersCreate () {
                         <option value="Hybrido">Hybrido</option>
                         <option value="Remoto">Remoto</option>
                     </select>            
-                    {errors.modality && <p>{errors.modality} </p>}            
                 </div>
 
-                <div className="">
+                <div className={styles.contenedor_inputs}>
+                    {(errors.min_salary || errors.max_salary) && <p>{errors.min_salary} </p>} 
                     <label >Salario: </label>
-                    <input className="" type='number' onChange={(event) => handleChange(event)} value={inputs.min_salary} name='min_salary'  placeholder="Salario maximo"/>                        
-                    <input className="" type='number' onChange={(event) => handleChange(event)} value={inputs.max_salary} name='max_salary'  placeholder="Salario minimo"/>
+                    <input className={styles.inputs_number} type='number' onChange={(event) => handleChange(event)} value={inputs.min_salary} name='min_salary'  placeholder="maximo"/>                        
+                    <input className={styles.inputs_number} type='number' onChange={(event) => handleChange(event)} value={inputs.max_salary} name='max_salary'  placeholder="minimo"/>
                 </div>
-                {(errors.min_salary || errors.max_salary) && <p>{errors.min_salary} </p>} 
-                <div>
+                <div className={styles.contenedor_inputs}>
                     <label >Link: </label>
-                    <input className="" type='text' onChange={(event) => handleChange(event)} value={inputs.link} name='link'  placeholder="ingrese link publicacion externa"/>                        
+                    <input className={styles.inputs_strings} type='text' onChange={(event) => handleChange(event)} value={inputs.link} name='link'  placeholder="ingrese link publicacion externa"/>                        
                 </div>
-                
-                <button className="" type='submit' disabled = {( Object.keys(errors).length !== 0 || inputs.title === '' )}> Publicar oferta</button>              
+                    <div>
+
+                    </div>
+                <button className={styles.boton_submit} type='submit' disabled = {( Object.keys(errors).length !== 0 || inputs.title === '' )}> Publicar oferta</button>              
             </form>
-            <Link to='/'> <button> Volver </button></Link>
+            <Link to='/'> <button className={styles.boton_volver}> Volver </button></Link>
         </div>
     )
 
