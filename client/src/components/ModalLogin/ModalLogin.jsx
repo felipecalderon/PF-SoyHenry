d0fa121d4658e2b61da840dadea4ec747b9a7715
Felipe <felipe.calderon321@gmail.com>
2023-03-19 11:18:00 -0300

    modal ready

--- client/src/components/ModalLogin/ModalLogin.jsx
+++ client/src/components/ModalLogin/ModalLogin.jsx
@@ -14,12 +14,12 @@ export const ModalLogin = ({isOpen, setOpen}) => {
     const contraseña = 'aprobado123'; // el login funciona como tal solamente con estos datos por el momento
     const navigate = useNavigate();
     const [access, setAccess] = useState(false);
-    const login = (form) => {
-        if(form.email === email && form.contraseña === contraseña){
-            setAccess(true);
-            navigate('/cards');
-        }
-    }
+    // const login = (form) => {
+    //     if(form.email === email && form.contraseña === contraseña){
+    //         setAccess(true);
+    //         navigate('/cards');
+    //     }
+    // }
 
     useEffect(() => {
         !access && navigate('/')
@@ -27,66 +27,56 @@ export const ModalLogin = ({isOpen, setOpen}) => {
 
     const [form, setForm] = useState({
         email: '',
-        contraseña: ''
+        password: ''
     });
 
     const [errors, setErrors] = useState({
         email: '',
-        contraseña:''
+        password:''
     });
 
     const closeModal = () => {
         setOpen(false)
     };
 
-    const handleEmail = (event) => {
+    const handleForm = (event) => {
         setForm({
             ...form,
-            email: (event.target.value)
+            [event.target.name]: (event.target.value)
         });
         setErrors(validations({
             ...form,
-            email: (event.target.value)
+            [event.target.name]: (event.target.value)
         }));
     };
 
-    const handleContraseña = (event) => {
-        setForm({
-            ...form,
-            contraseña: (event.target.value)
-        })
-        validations({
-            ...form,
-            contraseña: (event.target.value)
-        })
-    }
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        login(form);
+        alert('form enviado')
     };
 
     const handleModalContainerClick = (event) => event.stopPropagation(); //.stopPropagation hace que no se cierre el modal al hacer click dentro
 
     if(!isOpen) return null;
     return (
-        <div className='fixed top-0 left-0 right-0 bottom-0 bg-gray-800 bg-opacity-80 flex justify-center items-center' onClick={closeModal}>
-            <article className='w-1/2 mx-auto'>
-                <div className="bg-white rounded-3xl" onClick={handleModalContainerClick}>
+        <div className='z-50 fixed top-0 left-0 right-0 bottom-0 bg-gray-800 bg-opacity-80 flex justify-center items-center' onClick={closeModal}>
+            <article className='w-1/2 mx-auto relative'>
+                <div className="rounded-3xl bg-primary-light" onClick={handleModalContainerClick}>
+                <div className="absolute top-0 right-0 m-4 cursor-pointer text-2xl" onClick={closeModal}> x </div>
                 <h3 className="text-center font-medium text-2xl py-4">Ingresar a Fusionajob</h3>
                     <form onSubmit={handleSubmit} className="px-8 py-6">
                         <div className='mb-4'>
                           <label htmlFor="email" className='block text-gray-700 font-medium mb-2'>Email:</label>
-                          <input type='text' name='email' value={form.email} onChange={handleEmail} className='border-2 rounded-lg w-full px-3 py-2 text-gray-700' id="email" />
+                          <input type='text' name='email' value={form.email} onChange={handleForm} className='border-2 rounded-lg w-full px-3 py-2 text-gray-700' id="email" />
                           {errors.email && <p className='text-red-500 mt-2' >{errors.email}</p>}
                         </div>
                         <div className='mb-4'>
                           <label htmlFor="password" className='block text-gray-700 font-medium mb-2'>Contraseña:</label>
-                          <input type='password' name='password' value={form.contraseña} onChange={handleContraseña} className='border-2 rounded-lg w-full px-3 py-2 text-gray-700' id="password" />
-                          {errors.contraseña && <p className='text-red-500 mt-2' >{errors.contraseña}</p>}
+                          <input type='password' name='password' value={form.password} onChange={handleForm} className='border-2 rounded-lg w-full px-3 py-2 text-gray-700' id="password" />
+                          {errors.password && <p className='text-red-500 mt-2' >{errors.password}</p>}
                         </div>
                         <div className='flex flex-col w-auto pb-3'>
-                            <button className='bg-primary-dark hover:text-lg transition-all text-white font-medium py-2 px-4 rounded-md my-2 transition duration-200'>Ingresar</button>
+                            <button className='bg-primary-dark hover:text-lg transition-all text-white font-medium py-2 px-4 rounded-md my-2 duration-200'>Ingresar</button>
                             <button className='border-2 border-gray-400 hover:border-gray-500 text-gray-400 hover:text-gray-500 font-medium py-2 px-4 rounded-md my-2 transition duration-200'>Crear cuenta</button>
                         </div>
                         <div className='flex justify-center items-center text-sm'>
--- client/src/components/ModalLogin/validations.js
+++ client/src/components/ModalLogin/validations.js
@@ -7,11 +7,11 @@ const validations = (form) => {
     } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(form.email)) {
         errors.email = 'El email es invalido.';
     }
-    if(!form.contraseña.match(/\d/)){
-        errors.contraseña = "La contraseña debe contener al menos un número";
+    if(!form.password.match(/\d/)){
+        errors.password = "La contraseña debe contener al menos un número";
     }
-    if(form?.constraseña?.length < 6 || form?.contraseña?.length > 12){
-        errors.contraseña = "La contraseña debe contener entre 6 y 12 caracteres";
+    if(form?.password?.length < 6 || form?.password?.length > 12){
+        errors.password = "La contraseña debe contener entre 6 y 12 caracteres";
     }
     return errors;
 }
