d45a8a66d0b442520907fa359c41fc725cd4e52c
Felipe <felipe.calderon321@gmail.com>
2023-03-19 21:39:09 -0300

    cambiando fetch por axios en create user

--- api/src/app.js
+++ api/src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
     extended: true
 }))
 app.use(cors({
-    origin: 'https://fusionajobs-production.up.railway.app',
+    origin: '*',
     methods: '*'
   }))
 sequelize.sync({ force: true })
