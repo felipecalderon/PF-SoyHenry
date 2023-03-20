e24d9207c682e11d8f5e57859b430ae5d02f2758
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-03-20 09:40:50 -0500

    auth Google and new librarys

--- api/src/controllers/authControllers.js
+++ api/src/controllers/authControllers.js
@@ -1,5 +1,8 @@
 const { createUsers } = require("../handlers/handlerUserModels")
-const {firebase, googleProvider} = require('../auth/firebase.config')
+const { OAuth2Client } = require('google-auth-library');
+const { firebase, googleProvider } = require('../auth/firebase.config')
+require('firebase/auth');
+
 const authCreatePostulant = async (body) => {
     try {
         const { email, password } = body
@@ -14,11 +17,35 @@ const authCreatePostulant = async (body) => {
     }
 }
 
-const authLoginGoogle = async (body) => {
+const CLIENT_ID = '110189196879-qpnr4pooikg2o4nsgpnla6as3p48ffdn.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
+
+const authLoginGoogle = async () => {
   try {
-    await firebase.auth().signInWithRedirect(googleProvider)
-    return "Redirigiendo a Google para autenticación..."
+    // Obtener token de acceso de Google
+    const { tokens } = await client.getAccessToken();
+
+    // Usar el token de acceso para obtener el token de ID de Google
+    const googleIdToken = tokens.id_token;
+
+    // Iniciar sesión en Firebase con el token de ID de Google
+    const userCredential = await firebase.auth().signInWithCredential(
+      firebase.auth.GoogleAuthProvider.credential(googleIdToken)
+    );
+
+    // Obtener el usuario actual
+    const user = userCredential.user;
+
+    // Esperar a que se complete la autenticación con Firebase antes de obtener el código de verificación
+    await user.getIdTokenResult();
+
+    // Obtener el código de verificación para el usuario actual
+    const code = user.refreshToken;
+
+    // Devolver el usuario y el código de verificación
+    return { user, code };
   } catch (error) {
+    console.log(error);
     return `Error al autenticar con Google: ${error.message}`
   }
 }
