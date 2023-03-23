0e2e0725e4a20ae9a0c03f96701899b5c2409fc2
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-03-23 11:46:10 -0300

    edite favoritos y configuracion

--- client/src/components/UserProfile/Configuracion.jsx
+++ client/src/components/UserProfile/Configuracion.jsx
@@ -1,12 +1,63 @@
 import React from 'react'
 
+
 function Configuracion() {
+ 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+
   return (
-    <form>
-      <input type="text" />
-      <button>guardar</button>
-    </form>
-  )
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Nombre:</label>
+        <input type="text" id="name"  className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+      </div>
+      <div>
+        <label htmlFor="age">Edad:</label>
+        <input type="text" id="age" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"  />
+      </div>
+      <div>
+        <label htmlFor="location">Ubicación:</label>
+        <input type="text" id="location" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"  />
+      </div>
+      <div>
+        <label htmlFor="title">Título:</label>
+        <input type="text" id="title"  className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+      </div>
+      <div>
+        <label htmlFor="description">Descripción:</label>
+        <textarea id="description" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" ></textarea>
+      </div>
+      <div>
+        <label htmlFor="languages">Idiomas:</label>
+        <input type="text" id="languages" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"/>
+      </div>
+      <div>
+        <label htmlFor="skills">Habilidades:</label>
+        <input type="text" id="skills" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
+      </div>
+      <div>
+        <label htmlFor="phone">Telefono:</label>
+        <input type="text" id="phone" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"></input>
+      </div>
+      <div>
+        <label htmlFor="mail">Mail:</label>
+        <input type="mail" id="mail" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"></input>
+      </div>
+      <div>
+        <label htmlFor="social_media">Redes sociales:</label>
+        <input type="text" id="social_media" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"></input>
+      </div>
+      <div>
+        <label htmlFor="adress">Direccion:</label>
+        <input type="text" id="adress" className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm"></input>
+      </div>
+      
+      
+      </form>)
 }
 
 export default Configuracion
--- client/src/components/UserProfile/Favoritos.jsx
+++ client/src/components/UserProfile/Favoritos.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function Favoritos() {
+
+    const SVG_FAV = <svg width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+</svg>
+
     const data=[
         {
             "id": "java-developer-ssr-sr-leniolabs_-remote",
@@ -137,7 +142,7 @@ function Favoritos() {
             "bd_create": false
         },]
   return (
-    <>{data.reverse().map((el)=><>  <hr className='border border-b-gray-100  w-full ' /><h2>{el.title}</h2></>)}</>
+    <>{data.reverse().map((el)=><>  <hr className='border border-b-gray-100  w-full ' /><h2>{el.title}{SVG_FAV}</h2></>)}</>
   )
 }
 
