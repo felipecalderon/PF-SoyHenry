1873542416b433f8f03342c638ecc05e7d0ec6ae
Felipe <felipe.calderon321@gmail.com>
2023-03-27 08:32:13 -0300

    dashboard estilos y espacios

--- client/src/components/DashboardEmpresa/DashboardEmpresa.jsx
+++ client/src/components/DashboardEmpresa/DashboardEmpresa.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Footer from '../Footer/Footer'
 import { NavEmpresa } from '../NavEmpresa/NavEmpresa'
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Profile from "./Profile";
 import { CardsOfertasDb } from "./CardsOfertasDb";
 import axios from 'axios';
@@ -24,16 +24,17 @@ export const DashboardEmpresa = () => {
             })
     }, []) // eslint-disable-line
     return (
+        <>
         <div className="bg-primary-light dark:bg-secondary-dark">
             <NavEmpresa/>
-            <h2 className=' text-center py-6 mb-4 text-2x pr-10 font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white'>Dashboard Empresa</h2>
-            <Link to='/offerscreate' className='flex justify-center text-md mt-[1rem] pb-[1rem]'><button>Crear oferta</button></Link>
-            <div className="flex flex-row justify-center">
+            <h2 className=' text-center pt-6 mb-4 text-2x font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white'>Dashboard Empresa</h2>
+            <div className="flex flex-row justify-center py-6">
                 <div className="w-1/2 pl-8"><Profile company={userdata?.Companies[0]}/></div>
             
                 <div className="w-1/2"><CardsOfertasDb offers={offers}/></div>
             </div>
-            <Footer/>
         </div>
+        <Footer/>
+        </>
     )
 }
