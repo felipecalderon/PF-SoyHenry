984cef397cd4f597fa032ed8681ceee00b979e3f
Santiago Pagge <santy.pagge@gmail.com>
2023-03-27 22:35:34 -0300

    Resumen de ofertas ya acomodado

--- client/src/components/DashboardEmpresa/DashboardEmpresa.jsx
+++ client/src/components/DashboardEmpresa/DashboardEmpresa.jsx
@@ -29,15 +29,28 @@ export const DashboardEmpresa = () => {
     return (
         <>
         <div className="bg-primary-light dark:bg-secondary-dark">
-            <NavEmpresa/>
-            <h2 className=' text-center pt-6 mb-4 text-2x font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white'>Dashboard Empresa</h2>
-            <div className="flex flex-row justify-center py-6">
-                <div className="w-1/2 pl-8"><Profile company={userdata?.Companies[0]}/></div>
-                <div className="w-1/2 pt-[20rem]"><ResumenOfertas offers={offers}/></div>
-                <div className="w-1/2"><CardsOfertasDb offers={offers}/></div>
+            <NavEmpresa />
+            <h2 className="text-center pt-6 mb-4 text-2x font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">
+            Dashboard Empresa
+            </h2>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 py-6">
+                <div className="col-span-1 row-span-1 w-full md:w-1/2 pl-8">
+                <Profile company={userdata?.Companies[0]} />
+                </div>
+                <div className="col-span-1 row-span-2 flex md:block">
+                <div className="w-full h-full px-4">
+                    <CardsOfertasDb offers={offers} />
+                </div>
+                </div>
+                <div className="col-span-1 row-span-1 md:col-span-1 md:row-span-1 px-4">
+                <div className="pt-1">
+                    <ResumenOfertas offers={offers} />
+                </div>
+                </div>
+                <div className="col-span-2 row-span-1"></div>
             </div>
         </div>
-        <Footer/>
+            <Footer />
         </>
     )
 }
--- client/src/components/DashboardEmpresa/ResumenOfertas.jsx
+++ client/src/components/DashboardEmpresa/ResumenOfertas.jsx
@@ -5,16 +5,16 @@ export const ResumenOfertas = ({offers}) => {
     const totalApplications = offers.reduce((total, offer) => total + offer.applications_count, 0);
 
     return (
-        <div className="bg-secondary-light dark:bg-primary-dark p-2 rounded-2xl border border-slate-900 dark:border-white dark:text-text-dark">
-            <h2 className="flex items-center justify-center text-lg font-bold">Resumen de ofertas</h2>
+        <div className="relative w-[26rem] h-[10rem] bg-secondary-light dark:bg-primary-dark p-5 pt-1 rounded-2xl border border-slate-900 dark:border-white dark:text-text-dark">
+            <h2 className="flex items-center justify-center text-lg font-bold mb-[1rem]">Resumen de ofertas</h2>
             {offers.length === 1
-            ? <h3 className="flex items-center justify-center">{offers.length} Oferta creada</h3>
-            : <h3 className="flex items-center justify-center">{offers.length} Ofertas creadas</h3>}
+            ? <h3 className="flex items-center justify-center font-semibold">{offers.length} Oferta creada</h3>
+            : <h3 className="flex items-center justify-center font-semibold">{offers.length} Ofertas creadas</h3>}
             {totalApplications === 1
-            ? <h3 className="flex items-center justify-center">{totalApplications} Postulante</h3>
-            : <h3 className="flex items-center justify-center">{totalApplications} Postulantes</h3>}
-            <h3 className="flex items-center justify-center">Ofertas vencidas</h3>
-            <h3 className="flex items-center justify-center">Ofertas por caducar</h3>
+            ? <h3 className="flex items-center justify-center font-semibold">{totalApplications} Postulante</h3>
+            : <h3 className="flex items-center justify-center font-semibold">{totalApplications} Postulantes</h3>}
+            <h3 className="flex items-center justify-center font-semibold">Ofertas vencidas</h3>
+            <h3 className="flex items-center justify-center font-semibold">Ofertas por caducar</h3>
         </div>
     )
 }
