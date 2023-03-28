f72fdbd10e1b397da584ceb6141e058705ab8dde
Santiago Pagge <santy.pagge@gmail.com>
2023-03-27 22:54:16 -0300

    Resumen de ofertas todo funcional ya acomodado

--- client/src/components/DashboardEmpresa/ResumenOfertas.jsx
+++ client/src/components/DashboardEmpresa/ResumenOfertas.jsx
@@ -4,6 +4,29 @@ export const ResumenOfertas = ({offers}) => {
 
     const totalApplications = offers.reduce((total, offer) => total + offer.applications_count, 0);
 
+    // Filtrar ofertas que han caducado (más de 20 días desde su creación)
+    const ofertasCaducadas = offers.filter((offer) => {
+    const fechaCreacion = new Date(offer.createdAt);
+    const hoy = new Date();
+    const diasDiferencia = Math.floor(
+      (hoy.getTime() - fechaCreacion.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    return diasDiferencia > 20;
+  });
+
+    // Filtrar ofertas que están por caducar (menos de 5 días desde su caducidad)
+    const ofertasPorCaducar = offers.filter((offer) => {
+    const fechaCaducidad = new Date(offer.createdAt);
+    fechaCaducidad.setDate(fechaCaducidad.getDate() + 20);
+    // fechaCaducidad.setDate(fechaCaducidad.getDate() + 5); // Habilitar esta linea y deshabilitar la de arriba para hacer la prueba
+    const hoy = new Date();
+    const diasDiferencia = Math.floor(
+      (fechaCaducidad.getTime() - hoy.getTime()) / (1000 * 60 * 60 * 24)
+    );
+    return diasDiferencia <= 5;
+  });
+
+
     return (
         <div className="relative w-[26rem] h-[10rem] bg-secondary-light dark:bg-primary-dark p-5 pt-1 rounded-2xl border border-slate-900 dark:border-white dark:text-text-dark">
             <h2 className="flex items-center justify-center text-lg font-bold mb-[1rem]">Resumen de ofertas</h2>
@@ -13,8 +36,12 @@ export const ResumenOfertas = ({offers}) => {
             {totalApplications === 1
             ? <h3 className="flex items-center justify-center font-semibold">{totalApplications} Postulante</h3>
             : <h3 className="flex items-center justify-center font-semibold">{totalApplications} Postulantes</h3>}
-            <h3 className="flex items-center justify-center font-semibold">Ofertas vencidas</h3>
-            <h3 className="flex items-center justify-center font-semibold">Ofertas por caducar</h3>
+            {ofertasCaducadas === 1 
+            ? <h3 className="flex items-center justify-center font-semibold">{ofertasCaducadas.length} Oferta caducada</h3>
+            : <h3 className="flex items-center justify-center font-semibold">{ofertasCaducadas.length} Ofertas caducadas</h3>}
+            {ofertasPorCaducar === 1
+            ? <h3 className="flex items-center justify-center font-semibold">{ofertasPorCaducar.length} Oferta por caducar</h3>
+            : <h3 className="flex items-center justify-center font-semibold">{ofertasPorCaducar.length} Ofertas por caducar</h3>}
         </div>
     )
 }
