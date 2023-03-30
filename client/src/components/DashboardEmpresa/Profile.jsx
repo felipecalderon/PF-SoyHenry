e6692635f3d68f44847f864d0b09f6e5e22a43a6
Santiago Pagge <santy.pagge@gmail.com>
2023-03-30 18:59:39 -0300

    pequeños cambios

--- client/src/components/DashboardEmpresa/Profile.jsx
+++ client/src/components/DashboardEmpresa/Profile.jsx
@@ -7,23 +7,26 @@ import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import validationsDatosEmpresa from './validationsDatosEmpresa';
 import ModalConfirmChangesCompany from './ModalConfirmChangesCompany';
+import FormControl from '@mui/material/FormControl';
 
-const Transition = React.forwardRef(function Transition(props, ref) {
+  const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
   });
 
-const Profile = ({company}) => {
+  const Profile = ({company, user}) => {
   const [open, setOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
   
   //eslint-disable-next-line no-unused-vars
-  const {id, companyname, logo, location, website, phone, likes_count} = company
+  const {id, companyname, email_company, logo, location, website, phone_company, likes_count} = company;
+  const {city, country} = user;
   
   const [info, setInfo] = useState({
 		companyname: company.companyname,
+    email_company: company.email_company,
 		description: company.description,
 		location: company.location,
-		phone: company.phone,
+		phone_company: company.phone_company,
 		website: company.website,
 		logo: company.logo,
 		likes_count: company.likes_count
@@ -31,13 +34,14 @@ const Profile = ({company}) => {
 
   const [errors, setErrors] = useState({
     companyname: '',
+    email_company: '',
     description: '',
     location: '',
-    phone: '',
+    phone_company: '',
     website: '',
     logo: '',
     likes_count: '',
-});
+  });
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -52,7 +56,7 @@ const Profile = ({company}) => {
         ...info,
         [event.target.name]: event.target.value
     });
-};
+  };
 
 
 const handleSubmit = async (event) => {
@@ -73,26 +77,29 @@ const handleSubmit = async (event) => {
     <>
       <Box className="flex flex-row">
         <CardMedia
-          className='rounded-3xl ml-[3rem]'
+          className='ml-[3rem]'
           component="img"
           sx={{ width: 200, objectFit: 'contain' }}
           image={logo}
           alt="Live from space album cover"
         />
         <CardContent className="flex flex-col justify-center">
-          <Typography component="div" variant="h5" className='text-gray-900 dark:text-white'>
+          <Typography component="div" variant="h4" className='text-gray-900 dark:text-white'>
             {companyname}
+          </Typography>
+          <Typography component="div" variant="h6" className='text-gray-900 dark:text-white'>
+            <p className='text-sm'>{city}, {country}</p>
           </Typography>
           <Typography component="div" variant="subtitle1" className='text-blue-600'>
             <a href={website} target="_blank" rel="noopener noreferrer">{website}</a>
           </Typography>
-          <Rating name="ratingCompany" value={4} readOnly />
           <Typography component="div" variant="subtitle1" className='text-black-600 dark:text-white'>
             <p target="_blank" rel="noopener noreferrer">{location}</p>
           </Typography>
           <Typography component="div" variant="subtitle1" className='text-black-600 dark:text-white'>
-            <p target="_blank" rel="noopener noreferrer">{phone}</p>
+            <p target="_blank" rel="noopener noreferrer">{phone_company}</p>
           </Typography>
+          <Rating name="ratingCompany" value={4} readOnly />
           <Typography component="div" variant="subtitle1" className='text-black-600 dark:text-white'>
             <p target="_blank" rel="noopener noreferrer">Likes: {likes_count}</p>
           </Typography>
@@ -121,17 +128,89 @@ const handleSubmit = async (event) => {
           </DialogContentText>
           <form>
           <div>
-              <TextField label="Nombre de la empresa" value={info.companyname} onChange={handleChange} error={!!errors.companyname} helperText={errors.companyname} variant="standard" name='companyname' />
+              <TextField 
+                label="Nombre de la empresa" 
+                value={info.companyname} 
+                onChange={handleChange} 
+                error={!!errors.companyname} 
+                helperText={errors.companyname} 
+                variant="standard" 
+                name='companyname'/>
           </div>
           <div>
-              <TextField label="Logo" value={info.logo} onChange={handleChange} error={!!errors.logo} helperText={errors.logo} variant="standard" name='logo' />
+              <TextField 
+                label="Email de la empresa" 
+                value={info.email_company} 
+                onChange={handleChange} 
+                error={!!errors.email_company} 
+                helperText={errors.email_company} 
+                variant="standard" 
+                name='email_company'/>
           </div>
           <div>
-              <TextField label="Website" value={info.website} onChange={handleChange} error={!!errors.website} helperText={errors.website} variant="standard" name='website' />
+              <TextField 
+                label="Logo" 
+                value={info.logo} 
+                onChange={handleChange}
+                error={!!errors.logo} 
+                helperText={errors.logo} 
+                variant="standard" 
+                name='logo'/>
           </div>
           <div>
-              <TextField label="Ubicación" value={info.location} onChange={handleChange} error={!!errors.location} helperText={errors.location} variant="standard" name='location' />
+              <TextField 
+                label="Website" 
+                value={info.website} 
+                onChange={handleChange} 
+                error={!!errors.website} 
+                helperText={errors.website} 
+                variant="standard" 
+                name='website'/>
           </div>
+          {/* <div>
+          <FormControl variant="standard" sx={{ '& > :not(style)': { m: 1, width: '30ch' } }} >
+            <InputLabel id="demo-simple-select-standard-label"> País </InputLabel>
+              <Select
+                labelId="demo-simple-select-standard-label"
+                id="demo-simple-select-standard"
+                value={selectedCountry}
+                onChange={handleCountryChange}
+                MenuProps={MenuProps}
+                label="País"
+                name='country'>
+                  {
+                    countryData?.map((country) => (
+                      <MenuItem key={country.country} value={country.country}>
+                        {country.country}
+                      </MenuItem>
+                    ))
+                  }
+              </Select>
+          </FormControl>
+          </div>
+        <div className="mr-4 my-4">
+            <FormControl variant="standard" sx={{ '& > :not(style)': { m: 1, width: '30ch' } }} >
+                <InputLabel id="demo-simple-select-standard-label"> Ciudad </InputLabel>
+                <Select
+                  labelId="demo-simple-select-standard-label"
+                  id="demo-simple-select-standard"
+                  value={selectedCity}
+                  onChange={handleCityChange}
+                  MenuProps={MenuProps}
+                  label="Ciudad"
+                  name='city'
+                  disabled={!selectedCountry} 
+                  className="form-input mt-1 block rounded-md border-gray-300 shadow-sm w-full mx-2 text-base">
+                    {
+                    filteredCities?.map((city) => (
+                      <MenuItem key={city} value={city}>
+                        {city}
+                      </MenuItem>
+                    ))
+                  }
+                </Select>
+            </FormControl>
+          </div>   */}
           <div>
               <TextField label="Descripción" value={info.description} onChange={handleChange} error={!!errors.description} helperText={errors.description} variant="standard" name='description' />
           </div>
@@ -142,16 +221,19 @@ const handleSubmit = async (event) => {
           <Button onClick={() => setShowModal(true)}>Aceptar</Button>
         </DialogActions>
         <ModalConfirmChangesCompany isVisible={showModal} onClose={() => setShowModal(false)} >
-                <h1 className='flex font-bold justify-center p-3 dark:text-text-dark'>Antes de confirmar, verifique los datos</h1>
-                <h1 className='p-1 dark:text-text-dark'><strong>Nombre de la empresa:</strong> {info.companyname}</h1>                
-                <h1 className='p-1 dark:text-text-dark'><strong>Logo:</strong> {info.logo}</h1>
-                <h1 className='p-1 dark:text-text-dark'><strong>Website:</strong> {info.website}</h1>
-                <h1 className='p-1 dark:text-text-dark'><strong>Ubicacion:</strong> {info.location}</h1>
-                <h1 className='p-1 dark:text-text-dark'><strong>Descripción:</strong> {info.description} </h1>
-                <div className='flex justify-center p-5'>
-                  <button className='h-10 w-24 bg-gray-300 text-black dark:bg-slate-500 dark:text-white font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2' type='submit' onClick={handleSubmit}>Confirmar</button>
-                </div>            
-            </ModalConfirmChangesCompany>
+          <h1 className='flex font-bold justify-center p-3 dark:text-text-dark'>Antes de confirmar, verifique los datos</h1>
+          <h1 className='p-1 dark:text-text-dark'><strong>Nombre de la empresa:</strong> {info.companyname}</h1>                
+          <h1 className='p-1 dark:text-text-dark'><strong>Email de la empresa:</strong> {info.email_company}</h1>                
+          <h1 className='p-1 dark:text-text-dark'><strong>Logo:</strong> {info.logo}</h1>
+          <h1 className='p-1 dark:text-text-dark'><strong>Website:</strong> {info.website}</h1>
+          <h1 className='p-1 dark:text-text-dark'><strong>Ubicacion:</strong> {info.location}</h1>
+          <h1 className='p-1 dark:text-text-dark'><strong>Descripción:</strong> {info.description} </h1>
+          <div className='flex justify-center p-5'>
+            <button className='h-10 w-24 bg-gray-300 text-black dark:bg-slate-500 dark:text-white font-semibold rounded-lg shadow-md hover:bg-gray-400 focus:outline-none focus:ring-2' 
+              type='submit' 
+              onClick={handleSubmit}>Confirmar</button>
+          </div>            
+        </ModalConfirmChangesCompany>
       </Dialog>
     </>
     )
