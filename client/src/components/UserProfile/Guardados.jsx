36486227b16fddf459a5f0565939794db2dfaf69
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-03-31 20:21:03 -0300

    funciona guardados

--- client/src/components/UserProfile/Guardados.jsx
+++ client/src/components/UserProfile/Guardados.jsx
@@ -57,7 +57,7 @@ function Favoritos() {
                     <TurnedInIcon  />
                   </Fab>
                   <Link to={`/detail/${offer.offerId}?title=${offer.title}`} className='text-3xl dark:text-white font-bold'>
-                    {offer.title}
+                    {offer.hasOwnProperty("Offer") ? offer.Offer.title : offer.title}
                   </Link>
                 </h2>
               </Box>
