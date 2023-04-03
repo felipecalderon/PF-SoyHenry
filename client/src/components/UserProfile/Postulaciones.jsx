d8132edcdae9c325142ecd0d170ec4f8b0f213f2
Franco Valdez <franco.martin.valdez@mi.unc.edu.ar>
2023-04-03 15:54:00 -0300

    se agregaron  correciones a postulaciones y Config

--- client/src/components/UserProfile/Postulaciones.jsx
+++ client/src/components/UserProfile/Postulaciones.jsx
@@ -129,15 +129,15 @@ function Postulaciones() {
           </div>
         </>
       ) : (
-        <div className="flex flex-col justify-around w-full h-2/3">
-          <h2 className="text-3xl font-bold text-white mb-3 text-center">
-            Aún no te has postulado a ningún trabajo{" "}
+        <div className="flex flex-col justify-start w-full h-2/3">
+          <h2 className="text-3xl font-bold text-white mt-3 mb-3 text-center">
+            Aún no te has postulado a ningún trabajo
           </h2>
-          <p className="text-gray-400 text-2xl text-center">
+          <p className="text-gray-400 text-2xl mt-3 mb-3 text-center">
             ¡Explora las oportunidades de trabajo disponibles y postúlate para
             empezar a construir tu carrera!
           </p>
-          <img src={logofusionajob} alt="logo" className="text-center" />
+          <img src={logofusionajob} alt="logo" className="text-center mt-3 mb-3" />
         </div>
       )}
     </>
