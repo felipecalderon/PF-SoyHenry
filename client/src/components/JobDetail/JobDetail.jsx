dd4192bc213adc812fd77066f89b20fcabda1542
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-03 11:46:45 -0300

    deshabilita botn aplicar para usuario free

--- client/src/components/JobDetail/JobDetail.jsx
+++ client/src/components/JobDetail/JobDetail.jsx
@@ -285,7 +285,7 @@ useEffect(() => {
             <div className="mt-8 flex justify-center">
                 {
                   Number(jobId?.id)
-                    ? <button onClick={handlePostulateDb} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800" disabled={!dataUser?.premium} >
+                    ? <button onClick={handlePostulateDb} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"  disabled={!dataUser?.premium} style={{opacity: dataUser?.premium ? 1 : 0.5, cursor: dataUser?.premium ? 'pointer' : 'not-allowed'}}>
                     <span className="relative px-5 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                       Aplicar
                     </span>
