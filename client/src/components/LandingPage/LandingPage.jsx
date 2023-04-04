78fe955ea793d29a9f50d165ece65aed0eb750c6
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-03 21:12:03 -0300

    correcion en landingPage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -171,7 +171,7 @@ function LandingPage() {
               </li>
               <li className="flex space-x-3">
                 <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-blue-50 dark:bg-primary-dark" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Poder ver el estaado de todas las ofertas a la que aplicaste</span>
+                <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Poder ver el estado de todas las ofertas a la que aplicaste</span>
               </li>
             </ul>
             <button type="button" className="text-gray-800 bg-white hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Elegir cuenta premium</button>
