ec6b647f9f5f7ed77ad2f888ccbd31bcbe216e2b
Lisi Gonzalez <lisigonzalez@outlook.com>
2023-04-04 18:02:36 -0300

    cambios en landingPage

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -109,12 +109,12 @@ function LandingPage() {
       <Testimonials />
 
       <section className="py-10 w-full flex flex-col flex-wrap bg-primary-light dark:bg-secondary-dark">
-        <h2 className="pt-3 text-center mb-4 text-2x font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">¡Suscríbete!</h2>
+        <h2 className="pt-3 text-center mb-4 text-2x font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-5xl dark:text-white">¡Hazte Premium para disfrutar de estos beneficios!</h2>
         <div className='flex flex-col items-center md:flex-row justify-center gap-6'>
           <div className="w-full max-w-sm p-4 bg-white border border-black rounded-lg shadow p-4 sm:p-8">
             <h5 className="mb-4 text-xl font-medium text-gray-800 dark:text-gray-400">Plan Básico</h5>
             <div className="flex items-baseline text-gray-900 dark:text-">
-              <span className="text-2xl font-extrabold tracking-tight">Free</span>
+              <span className="text-5xl tracking-tight">Free</span>
             </div>
             <ul className="space-y-5 my-7">
               <li className="flex space-x-3">
@@ -141,9 +141,7 @@ function LandingPage() {
                 <svg aria-hidden="true" className="flex-shrink-0 w-5 h-5 text-gray-300 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Check icon</title><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                 <span className="text-base font-normal leading-tight text-gray-500">Poder ver el estado de todas las ofertas a la que aplicaste</span>
               </li>
-
             </ul>
-            <button type="button" className="text-gray-800 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center border border-gray-300">Elegir cuenta free</button>
 
           </div>
           <div className="w-full max-w-sm p-4 bg-secondary-light rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
@@ -179,7 +177,6 @@ function LandingPage() {
                 <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">Poder ver el estado de todas las ofertas a la que aplicaste</span>
               </li>
             </ul>
-            <button type="button" className="text-gray-800 bg-white hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-blue-200 dark:focus:ring-blue-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Elegir cuenta premium</button>
           </div>
 
         </div>
